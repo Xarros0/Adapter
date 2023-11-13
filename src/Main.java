cc63643edc607eea4911c52f097603dfd999f791
Jeremia <103106203+Xarros0@users.noreply.github.com>
2023-11-14 00:40:49 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        OldService oldService = new OldService();
+
+        NewService normalImplementation = new NormalImplementation(oldService);
+        NewService adapterImplementation = new Adapter(oldService);
+
+        Client client = new Client();
+        client.useService(normalImplementation);
+        client.useService(adapterImplementation);
+    }
+}
